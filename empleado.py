dd7aebaf1f6c5f0a8a8453ecc074aeb2d11f2e45
FernandoCR1976 <fernandocoralesm76@gmail.com>
2025-06-16 20:48:51 -0600

    programa completo con las opciones de modificar y borrar

--- empleado.py
+++ empleado.py
@@ -57,12 +57,64 @@ class SistemaRegistroEmpleados:
         for id_empleado, empleado in self.empleados.items():
             print(empleado.obtener_informacion())
 
+    def borrar_empleado(self):
+        print('\n --- Borrar Empleado ---')
+        if not self.empleados:
+            print('No hay empleado registrados')
+            return
+        id_a_borrar = input('Ingrese el ID del empleado a BORRAR:\n')
+        if id_a_borrar in self.empleados:
+            empleado_borrado = self.empleados.pop(id_a_borrar)
+            print(f'Empleado {empleado_borrado.nombre} {empleado_borrado.apellido} ha sido ELIMINADO')
+
+    def modificar_empleado(self):
+        print('\n --- Modificar Empleado ---')
+        if not self.empleados:
+            print('No hay empleado registrados')
+            return
+        id_a_modificar = input('Ingrese el ID del empleado a MODIFICAR:\n')
+        if id_a_modificar in self.empleados:
+            empleado = self.empleados[id_a_modificar]
+            print(f'Informacion del empleado actual: \n {empleado.obtener_informacion()}')
+            print('\nIngrese los nuevos datos del empleado (deje en blanco para mantener la informacion)')
+            nuevo_nombre = input(f'Nuevo Nombre: ({empleado.nombre})').strip()
+            if nuevo_nombre:
+                empleado.nombre = nuevo_nombre
+            
+            nuevo_apellido = input(f'Nuevo Apellido: ({empleado.apellido})').strip()
+            if nuevo_apellido:
+                empleado.apellido = nuevo_apellido
+            
+            nuevo_puesto = input(f'Nuevo Puesto: ({empleado.puesto})').strip()
+            if nuevo_puesto:
+                empleado.puesto = nuevo_puesto
+
+            while True:
+                nuevo_salario_str = input(f'Nuevo Salario ({empleado.salario:.2f}): ').strip()
+                if not nuevo_salario_str:
+                    break
+                try:
+                    nuevo_salario = float(nuevo_salario_str)
+                    if nuevo_salario < 0:
+                        print('El salario no puede ser ni 0 ni negativo. Intente de nuevo \n')
+                    else:
+                        empleado.salario = nuevo_salario
+                        break
+                except ValueError:
+                    print('Entrada invalida, por favor ingrese un valor valido para salario')
+            print(f'Empleado con la ID "{id_a_modificar}" ha sido modificado con exito')
+            print(f'Nueva informacion: {empleado.obtener_informacion()}')
+
+            
+
 
 def mostrar_menu():
     print("\n--- Menú Principal del Registro de Empleados ---")
     print("1. Agregar Nuevo Empleado")
     print("2. Listar Todos los Empleados")
-    print("3. Salir")
+    print("3. Borrar empleado")
+    print("4. Modificar empleado")    
+    print("5. Salir")
     print("---------------------------------------------")
 
 def main():
@@ -77,6 +129,10 @@ def main():
         elif opcion == '2':
             sistema_registro.listar_empleados()
         elif opcion == '3':
+            sistema_registro.borrar_empleado()
+        elif opcion == '4':
+            sistema_registro.modificar_empleado()        
+        elif opcion == '5':
             print('\n---SALIENDO DEL SISTEMA--')
             break
         else:
